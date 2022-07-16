aa8a49f1ac64e7216fb716b688470941f1684666
Carlos Andres Rivera <cr4185320@gmail.com>
2022-07-16 01:30:19 -0500

    Implemented stretchy buffers

--- code/oda.c
+++ code/oda.c
@@ -3,9 +3,64 @@
  * Date: 07/15/2022
  * File: oda.c
  * --------------------------------------------------------------------*/
+#include <stdlib.h>
 #include <stdio.h>
+#include <stddef.h>
+#include <assert.h>
+#include <stdbool.h>
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+typedef struct {
+	size_t len;
+	size_t cap;
+	char buf[0];
+} BufHdr;
+
+#define buf__hdr(b) ((BufHdr *)((char *)(b) - offsetof(BufHdr, buf)))
+#define buf__fits(b, n) (buf_len(b) + n <= buf_cap(b))
+#define buf__fit(b, n) (buf__fits(b, n) ? 0 : ((b) = buf__grow(b, buf_len(b) + n, sizeof(*(b)))))
+
+#define buf_len(b) ((b) ? buf__hdr(b)->len : 0)
+#define buf_cap(b) ((b) ? buf__hdr(b)->cap : 0)
+#define buf_end(b) ((void *)((char *)(b) + sizeof(*(b)) * buf_len((b))))
+#define buf_push(b, ...) (buf__fit((b), 1), (b)[buf__hdr(b)->len++] = (__VA_ARGS__))
+#define buf_free(b) ((b) ? free(buf__hdr(b)) : 0)
+
+void *buf__grow(const void *buf, size_t new_len, size_t element_size) {
+	size_t new_cap = MAX(1 + 2 * buf_cap(buf), new_len);
+	assert(new_len <= new_cap);
+	size_t new_size = new_cap * element_size + offsetof(BufHdr, buf);
+	BufHdr *new_hdr = NULL;
+	if (buf) {
+		new_hdr = realloc(buf__hdr(buf), new_size);
+
+	} else {
+		new_hdr = malloc(new_size);
+		new_hdr->len = 0;
+	}
+	new_hdr->cap = new_cap;
+
+	return new_hdr->buf;
+}
+
+void buf_test() {
+	int *foo = NULL;
+
+	for (int i = 0; i < 1024; i++) {
+		buf_push(foo, i);
+	}
+	assert(buf_len(foo) == 1024);
+
+	for (int i = 0; i < 1024; i++) {
+		assert(foo[i] == i);
+	}
+
+	buf_free(foo);
+}
 
 int main(int argc, char **argv) {
-	printf("Hello, world\n");
+	buf_test();
+
 	return 0;
 }
