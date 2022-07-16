e2b8285e31d915f2106e3849d8e7223b48d79e3a
Carlos Andres Rivera <cr4185320@gmail.com>
2022-07-15 23:36:40 -0500

    Create oda.c

---
+++ code/oda.c
@@ -0,0 +1,11 @@
+/* ----------------------------------------------------------------------
+ * Author: Carlos Rivera
+ * Date: 07/15/2022
+ * File: oda.c
+ * --------------------------------------------------------------------*/
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+	printf("Hello, world\n");
+	return 0;
+}
