8c7ad8f4c915cd84ef31223e8dd8e5605a0fe65d
Carlos Andres Rivera <cr4185320@gmail.com>
2022-07-22 20:27:26 -0500

    Update build.bat

--- code/build.bat
+++ code/build.bat
@@ -3,5 +3,5 @@
 SET warning_flags=-W4 -WX -wd4201 -wd4053 -wd4100 -wd4189 -wd4200
 PUSHD ..\build
 
-CL -nologo -FC -Zi -MDd %warning_flags% ..\code\oda.c
+CL -nologo -FC -Zi -MDd %warning_flags% ..\code\main.c -Feoda.exe
 POPD
